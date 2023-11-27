67caf3692566362136d6e8f6f60f99beef7f7018
Marcin Słomka <m.slomka@student.po.edu.pl>
2023-11-27 15:14:56 +0100

    Add project files.

---
+++ Bookkeeping/Invoice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace dksApp
+{
+    public class Invoice
+    {
+        public int Id { get; set; } //NR.
+        public string Type { get; set; }
+        public string SellerName { get; set; } //Sprzedawca
+        public string SellerAddress { get; set; }
+        public string SellerNIP { get; set; }
+        public string SellerPhoneNumber { get; set; }
+        public string SellerEmail { get; set; }
+        public string BuyerName { get; set; } // Nabywca
+        public string BuyerAddress { get; set; }
+        public string BuyerNIP { get; set; }
+        public string BuyerPhoneNumber { get; set; }
+        public string BuyerEmail { get; set; }
+        public string Payment { get; set; } // Płatność
+        public decimal Price { get; set; } // Kwota
+        public DateTime Date { get; set; } // Data wystawienia
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using dksApp.Bookkeeping;
+
+namespace dksApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Navigate(new MainBookPage());
+        }
+
+        //#region navigate
+        private void Navigate(Page page)
+        {
+            MainContentFrame.NavigationService.Navigate(page);
+        }
+
+        private void NavigationToBookKeepingBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Navigate(new MainBookPage());
+        }
+
+        //#settingsfrontend
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left) 
+            {
+                this.DragMove();
+            }
+        }
+
+        private bool IsMaximized = false;
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) 
+        {
+            if(e.ClickCount == 2)
+            {
+                if(IsMaximized) 
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1080;
+                    this.Height = 720;
+
+                    IsMaximized = false;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+
+                    IsMaximized= true;
+                }
+            }
+        }
+
+        //Function's
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
